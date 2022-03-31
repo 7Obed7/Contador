dc0d47ee6b55cab45191b9c50ced94a1380ccf65
Lauro Obed Nolasco Vazquez <isc18350511@gmail.com>
2022-03-31 16:15:05 -0600

    Incrementando contador a través de un botón

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-const App= () => {
+import { useState } from "react";
+import "./app.css";
+
+const App = () => {
+  const [valor, SetValor] = useState(0);
+
+  const handleClick = (v) => {
+    SetValor(v+1);
+  };
+
   return (
     <div className="App">
-      <h3>Contador </h3>
+      <h3> Contador </h3>
+      <hr />
+      <h2 className="text-center">{valor}</h2>
+      <hr />
+      <div className="margin-button">
+        <button 
+        className="btn btn-primary me-2" 
+        onClick={() =>handleClick(valor)}
+        >
+          +1
+        </button>
       </div>
+    </div>
   );
-
-}
+};
 export default App;
